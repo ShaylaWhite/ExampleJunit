77257ed576162276268627b779b0e72091924fe0
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-05-24 08:41:22 -0700

    Initital Commit

---
+++ EampleJunit/src/main/java/com/qa/examplejunit/Runner.java
@@ -0,0 +1,8 @@
+package com.qa.examplejunit;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		System.outprintln("Hello Maven")	}
+
+}
---
+++ EampleJunit/src/test/java/com/qa/tezt/HouseTest.java
@@ -0,0 +1,12 @@
+package com.qa.tezt;
+
+import org.junit.Test;
+
+public class HouseTest {
+
+	@Test
+	public void Test1() {
+		
+	}
+
+}
